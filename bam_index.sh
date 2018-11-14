3ff5f5dca7705859feaa7efb21fc44a0bf2043d1
Jill Marzolino <jmarz001@ucr.edu>
2018-11-13 16:51:11 -0800

    Add files via upload

---
+++ bam_index.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+#SBATCH --ntasks=15
+#SBATCH --time=72:00:00
+#SBATCH --output=/rhome/jmarz001/bigdata/CCXXIRAD/Scripts/bam-index.stdout
+#SBATCH --job-name='index'
+#SBATCH -p koeniglab
+#SBATCH --array=1-384
+
+WORKINGDIR=/rhome/jmarz001/bigdata/CCXXIRAD/raw_aligns
+cd $WORKINGDIR
+SEQLIST=$WORKINGDIR/bams
+FILE=$(head -n $SLURM_ARRAY_TASK_ID $SEQLIST | tail -n 1)
+NAME=$(basename "$FILE" | cut -d. -f1)
+
+#samtools index [-bc] [-m INT] aln.bam|aln.cram [out.index]
+#Index a coordinate-sorted BAM or CRAM file for fast random access
+# -b Create a BAI index. This is currently the default when no format options are used.
+
+module load samtools
+samtools index -c $NAME.bam
