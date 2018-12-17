fdb228d009ffefffd94027e2b9cdc85445538332
Jill Marzolino <jmarz001@ucr.edu>
2018-12-17 09:32:46 -0800

    run with correct chromosome regions

--- pipeline/freebayes_snp.sh
+++ pipeline/freebayes_snp.sh
@@ -3,19 +3,22 @@
 #SBATCH --ntasks=20
 #SBATCH --time=168:00:00
 #SBATCH --mem=200G
-#SBATCH --output=/rhome/jmarz001/bigdata/CCXXIRAD/Scripts/freebayes.stdout
-#SBATCH --job-name='freebayes'
+#SBATCH --output=/rhome/jmarz001/bigdata/CCXXIRAD/Scripts/chr1.stdout
+#SBATCH --job-name='chr1_snps'
 #SBATCH -p koeniglab
 
 WORK=/rhome/jmarz001/bigdata/CCXXIRAD/align
 cd $WORK
-RESULTSDIR=/rhome/jmarz001/bigdata/CCXXIRAD/calls
+RESULT=/rhome/jmarz001/bigdata/CCXXIRAD/calls
+REF=/rhome/jmarz001/shared/GENOMES/NEW_BARLEY/GENOME_SPLIT/barley_split_reference.fa
 
-module load freebayes #freebayes/1.2.0(default)
+module load freebayes/1.2.0
 
 #freebayes -f [reference] [infiles.bam] > [outfiles.vcf]
-freebayes -k -f /rhome/jmarz001/bigdata/REF/Hordeum_vulgare.Hv_IBSC_PGSB_v2.dna.toplevel.fa \
-$WORK/*.bam > $RESULTSDIR/rad.snps.freebayes.vcf
+freebayes -k -f $REF -r chr1H_1_279267716 $WORK/*.bam > $RESULT/rad.snps.chr1_1.freebayes.vcf
 
 #population priors:
 #-k --no-population-priors Equivalent to --pooled-discrete and removal of Ewens Sampling Formula priors
+
+#-r --region <chrom>:<start_position>..<end_position>
+#Limit analysis to the specified region, 0-base coordinates, end_position not included (same as BED format).
