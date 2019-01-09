a285d45cdf8cc027cde303b54304d60dc4d37dec
Jill Marzolino <jmarz001@ucr.edu>
2019-01-09 12:03:43 -0800

    update to first rough filter

--- pipeline/filter.sh
+++ pipeline/filter.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/bash -l
 
-#SBATCH -p koeniglab
-#SBATCH --ntasks=5
+#SBATCH -p short
+#SBATCH --ntasks=2
 #SBATCH --mem=100G
-#SBATCH --time=168:00:00
-#SBATCH --output=/rhome/jmarz001/bigdata/CCXXIRAD/Scripts/qual_filter.stdout
+#SBATCH --time=2:00:00
+#SBATCH --output=/rhome/jmarz001/bigdata/CCXXIRAD/Scripts/bcf_filter.stdout
 #SBATCH --job-name='filter'
-#SBATCH --array=1-384%5
+#SBATCH --array=1-16
 
+# set directories
 WORK=/rhome/jmarz001/bigdata/CCXXIRAD/calls
 cd $WORK
-RESULT=/rhome/jmarz001/bigdata/CCXXIRAD/calls/filtered
-SEQS=$WORK/files
+RESULT=/rhome/jmarz001/bigdata/CCXXIRAD/calls/bcftools_filtered
+# define and generate file list
+SEQS=$WORK/seqs
+ls *.vcf > $SEQS
+
 FILE=$(head -n $SLURM_ARRAY_TASK_ID $SEQS | tail -n 1)
-NAME=$(basename "$FILE" | cut -d. -f1)
+NAME=$(basename "$FILE" | cut -d. -f1-2)
 
 module load bcftools/1.8
 # bcftools filter [options] <file>
-bcftools filter -e "QUAL < 30 || F_MISSING > 0.5" $FILE -o $RESULT/$NAME.filtered.vcf
-bcftools view -i "F_MISSING > 0.5" $FILE -o $RESULT/$NAME.missing
-#bcftools filter --regions-file $pathToBed/BEDFILE.bed $inputVCF | bcftools filter -e "QUAL < 10 || F_MISSING > 0.01 || MAF > 0.98" | bcftools view -m2 -M2 -v snps --output-type z --output-file $outputDIR/$name._filtered.vcf.gz | bcftools index -t --output-file $outputDIR/$name._filtered.vcf.gz.tbi
-# -q, --min-af FLOAT[:nref|:alt1|:minor|:major|:nonmajor
-# bcftools view -q 0.01:minor chr.all.vcf.gz
+
+bcftools view -i 'F_MISSING<0.5 & DP>0 & DP<4031 & QUAL>30 & N_ALT=1' $FILE -o $RESULT/$NAME.minalt.vcf
+
+# INCLUDE: sites missing less than 50%, depth higher than 0 and less than 4031,
+# N_ALT=1 makes only options ref/single alt allele (biallelic site instead of 4 alt alleles or something)
