0dcbbb381df2bbde2c42584258b23762c1ba2e75
Jill Marzolino <jmarz001@ucr.edu>
2019-03-21 17:47:02 -0700

    organization

--- statistics/snp_density.sh
+++ statistics/snp_density.sh
@@ -9,3 +9,4 @@
 
 module load vcftools/0.1.13
 vcftools --vcf /rhome/jmarz001/bigdata/CCXXIRAD/calls/bcftools_filtered/CCXXIRAD.ref_alt_ratio.vcf --SNPdensity 10kb --out CCXXIRAD_density
+vcftools --vcf /rhome/jmarz001/bigdata/CCXXIRAD/calls/bcftools_filtered/CCXXIRAD.ref_alt_ratio.vcf --SNPdensity 10000 --out CCXXIRAD_full_density
