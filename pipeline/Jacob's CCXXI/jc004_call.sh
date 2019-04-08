9676083f29469967605f22657c518ef420acc67b
Jill Marzolino <jmarz001@ucr.edu>
2019-04-08 11:15:07 -0700

    write freebayes snp caller

---
+++ pipeline/Jacob's CCXXI/jc004_call.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH --time=168:00:00
+#SBATCH --ntasks=10
+#SBATCH --mem-per-cpu=30G
+#SBATCH --output=jc004_call.stdout
+#SBATCH --job-name='jc004'
+#SBATCH -p koeniglab
+#SBATCH --array=1-86
+
+BAM=/rhome/jmarz001/bigdata/CCXXIRAD/CCXXIRAD2/align
+cd $BAM
+SNP=/rhome/jmarz001/bigdata/CCXXIRAD/CCXXIRAD2/calls
+REF=/rhome/jmarz001/shared/GENOMES/NEW_BARLEY/GENOME_SPLIT/barley_split_reference.fa
+
+CHR=/rhome/jmarz001/bigdata/CCXXIRAD/calls/chr_splits.txt
+REGION=$(head -n $SLURM_ARRAY_TASK_ID $CHR | tail -n 1)
+
+module load freebayes/1.2.0
+
+#freebayes -f [reference] [infiles.bam] > [outfiles.vcf]
+freebayes -k -f $REF -r $REGION $BAM/*.bam > $SNP/rad.$REGION.vcf
