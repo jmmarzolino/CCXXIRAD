7ae710b788ba45ed9bee0710c988151410165426
Jill Marzolino <jmarz001@ucr.edu>
2019-10-18 11:41:21 -0700

    include the freakin' RIGHT INDEX FILE

--- pipeline/combined_processing/cmd003_align.sh
+++ pipeline/combined_processing/cmd003_align.sh
@@ -8,16 +8,16 @@
 #SBATCH -p koeniglab
 #SBATCH --array=1-468%10
 
-module load minimap2 samtools
-INDEX=/rhome/jmarz001/shared/GENOMES/BARLEY/2019_Release_Morex_vers2/Barley_Morex_V2_pseudomolecules.fasta
+module load minimap2/2.17 samtools/1.9
 MINDEX=/rhome/jmarz001/shared/GENOMES/BARLEY/2019_Release_Morex_vers2/Barley.mmi # minimap2 index
+INDEX=/rhome/jmarz001/shared/GENOMES/BARLEY/2019_Release_Morex_vers2/Barley_Morex_V2_pseudomolecules.fasta
 PROJECT_DIR=/rhome/jmarz001/bigdata/CCXXIRAD/combined_CCXXI
 # Define location variables
 TRIMMED=$PROJECT_DIR/data/trimmed
-BAMS=${PROJECT_DIR}/data/bams
-mkdir $BAMS
+BAMS=$PROJECT_DIR/data/bams
+#mkdir $BAMS
 
-SEQS=${PROJECT_DIR}/args/trimmed_files
+SEQS=$PROJECT_DIR/args/trimmed_files
 cd $TRIMMED ; ls *trimmed.fq.gz >> $SEQS
 
 # Define files to run over
@@ -28,41 +28,34 @@ sample_name=$(basename "$FILE" | cut -d. -f1 | cut -d_ -f1-2)
 ##RGPU (String)	Read Group platform unit (eg. run barcode) Required.
 
 ##RGLB (String)	Read Group library Required.
-#RGLB=<"24=F25, 267=F11, 25=F29">
-#yields generation number
+# RGLB=<"24=F25, 267=F11, 25=F29">
+# yields generation number
 generation=$(basename "$FILE" | cut -d. -f1 | cut -d_ -f1)
 # KL numbers
-X=267 ; Y=24 ; Z=25
+X=267
+Y=24
+Z=25
 
-# if expression1 then statement1 else if expression2 then statement2 else statement3
-if [ "$generation" == "$X" ]; then
+if [ "$generation" == "$X" ]
+then
   RGLB=F11
-else if [ "$generation" == "$Y" ]; then
+elif [ "$generation" == "$Y" ]
+then
   RGLB=F25
-else if [ "$generation" == "$Z" ]; then
+elif [ "$generation" == "$Z" ]
+then
   RGLB=F29
 else
   RGLB=error
 fi
 
-# BWA mapping
-bwa mem -R "@RG\tID:${sample_name}_${SLURM_ARRAY_TASK_ID}\tSM:${sample_name}\tPU:"illumina"\tLB:$RGLB" -t 10 $INDEX $TRIMMED/${sample_name}_${run_name}.fq.gz > $BAMS/bwa_${sample_name}_${run_name}.sam
-
 # Minimap2 mapping
-minimap2 -t 10 -ax sr $MINDEX \
--R "@RG\tID:${sample_name}_${SLURM_ARRAY_TASK_ID}\tSM:${sample_name}\tPU:illumina\tLB:$RGLB" \
-$TRIMMED/${sample_name}_${run_name}.fq.gz \
-> $BAMS/mini_${sample_name}_${run_name}.sam
+#minimap2 -t 10 -ax sr $MINDEX -R "@RG\tID:${sample_name}_${SLURM_ARRAY_TASK_ID}\tSM:${sample_name}\tPU:illumina\tLB:$RGLB" $TRIMMED/$FILE > $BAMS/${sample_name}.sam
 
 # Get mapping stats
 mkdir $BAMS/mappingstats/
-samtools flagstat $BAMS/bwa_${sample_name}_${run_name}.sam > $BAMS/mappingstats/bwa_${sample_name}_${run_name}_mapstats.txt
-
-samtools flagstat $BAMS/mini_${sample_name}_${run_name}.sam > $BAMS/mappingstats/mini_${sample_name}_${run_name}_mapstats.txt
+samtools flagstat $BAMS/${sample_name}.sam > $BAMS/mappingstats/${sample_name}_mapstats.txt
 
 # Convert sam to sorted bam and index bams with csi
-samtools view -b -T $INDEX $BAMS/bwa_${sample_name}_${run_name}.sam | samtools sort -@ 20 > $BAMS/bwa_${sample_name}_${run_name}.bam
-samtools index -c $BAMS/bwa_${sample_name}_${run_name}.bam
-
-samtools view -b -T $INDEX $BAMS/mini_${sample_name}_${run_name}.sam | samtools sort -@ 20 > $BAMS/mini_${sample_name}_${run_name}.bam
-samtools index -c $BAMS/mini_${sample_name}_${run_name}.bam
+samtools view -b -T $INDEX $BAMS/${sample_name}.sam | samtools sort -@ 20 > $BAMS/${sample_name}.bam
+samtools index -c $BAMS/${sample_name}.bam
