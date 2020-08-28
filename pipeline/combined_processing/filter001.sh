66b3da946d921dce159a389139b20eba95f12479
Jill Marzolino <jmarz001@ucr.edu>
2020-08-28 12:50:24 -0700

    init

--- pipeline/combined_processing/filter001.sh
+++ pipeline/combined_processing/filter001.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/bash -l
 
 #SBATCH -p short
-#SBATCH --ntasks=2
-#SBATCH --mem=100G
+#SBATCH --ntasks=1
+#SBATCH --mem=65G
 #SBATCH --time=2:00:00
-#SBATCH --output=/rhome/jmarz001/bigdata/CCXXIRAD/combined_CCXXI/filter001.stdout
+#SBATCH --output=/rhome/jmarz001/bigdata/CCXXIRAD/combined_CCXXI/scripts/filter001.stdout
 #SBATCH --job-name='filter'
-#SBATCH --array=1-4
-#468%25
+#SBATCH --array=1-468%5
 
-module load bcftools/1.8
+module load bcftools/1.10 perl/5.24.0
 # Define directories
 PROJECT_DIR=/rhome/jmarz001/bigdata/CCXXIRAD/combined_CCXXI
 SNPS=$PROJECT_DIR/data/calls
 FILT=$SNPS/filter
-mkdir $FILT
+#mkdir $FILT
 # define file list
 SEQS=$PROJECT_DIR/args/vcf_files
-cd $SNPS ; ls *.vcf > $SEQS
+#cd $SNPS; ls *.vcf > $SEQS
 
 FILE=$(head -n $SLURM_ARRAY_TASK_ID $SEQS | tail -n 1)
-sample_name=$(basename "$FILE" | cut -d_ -f2-3)
+sample_name=$(basename $FILE | cut -d_ -f2-3)
 
 # get the stats you need!
-bcftools stats $SNPS/$FILE > $FILT/${sample_name}.stats
+#bcftools stats $SNPS/$FILE > $FILT/${sample_name}.stats
 
 # bcftools filter [options] <file>
-# INCLUDE: sites missing less than 50%, depth higher than 0 and less than 4031,
-# N_ALT=1 makes only options ref/single alt allele (biallelic site instead of 4 alt alleles or something)
-bcftools view -i 'F_MISSING<0.5 && DP>0 & DP<4031 && QUAL>30 && N_ALT=1' $SNPS/$FILE -o $FILT/${sample_name}_filt001.vcf
-# && COUNT(GT="het")<124 && COUNT(GT="alt")>0 && COUNT(GT="ref")>0
+bcftools view -i 'F_MISSING<0.5 && INFO/DP>0 && INFO/DP<4031 && QUAL>30 && N_ALT=1' $SNPS/$FILE -o $FILT/${sample_name}_filt001.vcf
+#&& COUNT(GT="het")<124 && COUNT(GT="alt")>0 && COUNT(GT="ref")>0
+
+bcftools stats $FILT/${sample_name}_filt001.vcf > $FILT/${sample_name}_filt001.stats
